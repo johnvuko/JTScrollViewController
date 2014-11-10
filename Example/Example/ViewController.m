2c0ad3b72ccff9f83bf63c2d7939552ced378203
Jonathan Tribouharet <jonathan@siu-soon.com>
2014-11-10 16:00:57 +0100

    Update Exmaple

--- Example/Example/ViewController.m
+++ Example/Example/ViewController.m
@@ -17,6 +17,22 @@
 {
     [super viewDidLoad];
     
+    {
+        CGFloat spaceFromTop = 10.;
+        
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, spaceFromTop, 0, 90)];
+        label.textAlignment = NSTextAlignmentCenter;
+        label.font = [UIFont systemFontOfSize:30];
+        label.text = @"JTScrollViewController";
+        
+        [self.contentView addSubview:label];
+        
+        UIView *separator = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0, 1)];
+        separator.backgroundColor = [UIColor grayColor];
+        
+        [self.contentView addSubview:separator];
+    }
+    
     for(int i = 0; i < 4; ++i){
         CGFloat spaceFromTop = 10.;
         
